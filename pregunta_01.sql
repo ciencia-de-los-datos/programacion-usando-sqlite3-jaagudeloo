8429afda66506aed6a8360476d26b1be98cbbda0
jaagudeloo <jaagudeloo@unal.edu.co>
2022-04-10 13:31:59 -0500

    Lab3_v1
    Lab3_v1

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,4 +17,22 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT sum(c12) FROM tbl1;
+
+import sqlite3
+import pandas as pd
+
+conexion = sqlite3.connect(':memory:')
+cursor = conexion.cursor()
+
+#1
+tbl1_pandas = pd.read_csv('tbl1.csv', header = None)
+tbl1_pandas.columns = ['K0', 'K1', 'c12', 'c13', 'c14', 'c15', 'c16']
+tbl1_pandas['c14'] = pd.to_datetime(tbl1_pandas['c14'], format='%Y-%m-%d')
+
+tbl1_pandas.to_sql(
+    name='tbl1',
+    con=conexion,
+    if_exists='replace',
+)
+
+cursor.execute('SELECT sum(c12) FROM tbl1;').fetchall()
