2320369a03fb894ee220be997b4ca9750e00da39
jaagudeloo <jaagudeloo@unal.edu.co>
2022-04-10 16:27:16 -0500

    Lab3_v4
    Lab3_v4

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -37,4 +37,6 @@
 --  0  2018     6
 --
 --  >>> Escriba su codigo a partir de este punto <<<
+--SELECT strftime("%Y", c14) AS YEAR, count(*) AS CANT FROM tbl1 WHERE strftime("%Y", c14) = '2018' GROUP BY strftime("%Y", c14);
+SELECT COUNT(*) FROM tbl1 WHERE strftime("%Y", c14) = '2018' GROUP BY strftime("%Y", c14);
 -- 
